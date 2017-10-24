d604d54401b71fb42f0e6d38c1de1e4bab6df48e
user_07 <user_07@mac-07deimac.local>
2017-10-24 17:38:20 +0800

    Initial Commit

---
+++ bill/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bill
+//
+//  Created by user_23 on 2017/4/7.
+//  Copyright © 2017年 user_23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bill/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bill
+//
+//  Created by user_23 on 2017/4/7.
+//  Copyright © 2017年 user_23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
